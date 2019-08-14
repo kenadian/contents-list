d43472a5f1e27438cfe5f8567da57b14bda55713
Ken Hare <kenhare@ultrapresence.com>
2019-08-14 17:48:19 -0600

    add some comments

--- src/reducers/reducer_contents.js
+++ src/reducers/reducer_contents.js
@@ -1,5 +1,6 @@
 import { ADD_ITEM, REMOVE_ITEM } from "../actions/contents";
 import { removeIn } from "immutable";
+
 const initialState = {
   total: 0,
   entities: {
@@ -24,6 +25,7 @@ const initialState = {
   }
 };
 
+// Generates next id when adding items to store
 function generateId(inputArray) {
   if (inputArray.length === 0) {
     return 0;
@@ -90,79 +92,46 @@ export default function(state = initialState, action) {
     case REMOVE_ITEM:
       const removeItemId = payload;
       const removeAmount = state.entities.items.byId[removeItemId].amount;
+      // Using immutables removeIn to simplify state management
+      // First remove from item.byId object
       let newState = removeIn(state, [
         "entities",
         "items",
         "byId",
         removeItemId
       ]);
+
+      // Next subtract the amount being removed from the grand total
       newState.total -= removeAmount;
+
+      // Filter out the removed id from the allIds array
       newState.entities.items.allIds = newState.entities.items.allIds.filter(
         element => element !== removeItemId
       );
+
+      // lookup the category id that needs to have its total updated
       const updateCategoryId =
         newState.entities.categoryItems.byId[removeItemId].categoryId;
 
+      // update the category total
       newState.entities.category.byId[updateCategoryId].total -= removeAmount;
 
+      // Again use the immutable.js library function to easily remove the
+      // appropriate property from the categoryItems lookup object
       newState = removeIn(newState, [
         "entities",
         "categoryItems",
         "byId",
         removeItemId
       ]);
+
+      // filter out the removed id from allIds
       newState.entities.categoryItems.allIds = newState.entities.categoryItems.allIds.filter(
         element => element !== removeItemId
       );
 
-      //   const items = state.entities.items;
-
-      //   const itemId = generateId(state.entities.items.allIds.slice(0));
-
-      //   const categoryItemsId = generateId(
-      //     state.entities.categoryItems.allIds.slice(0)
-      //   );
-
-      //   return {
-      //     ...state,
-      //     total: Number(state.total) + Number(amount),
-      //     entities: {
-      //       ...state.entities,
-      //       category: {
-      //         ...state.entities.category,
-      //         byId: {
-      //           ...state.entities.category.byId,
-      //           [categoryId]: {
-      //             ...state.entities.category.byId[categoryId],
-      //             total:
-      //               Number(state.entities.category.byId[categoryId].total) +
-      //               Number(amount)
-      //           }
-      //         }
-      //       },
-      //       items: {
-      //         byId: {
-      //           ...items.byId,
-      //           [itemId]: {
-      //             name,
-      //             amount
-      //           }
-      //         },
-      //         allIds: [...items.allIds, itemId]
-      //       },
-      //       categoryItems: {
-      //         byId: {
-      //           ...state.entities.categoryItems.byId,
-      //           [categoryItemsId]: {
-      //             id: categoryItemsId,
-      //             itemId,
-      //             categoryId: Number(categoryId)
-      //           }
-      //         },
-      //         allIds: [...state.entities.categoryItems.allIds, categoryItemsId]
-      //       }
-      //     }
-      //   };
+      // return the new state object.
+      // And done.
       return newState;
     default:
       return state;
