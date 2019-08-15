7d100cad0b72156d00dbbf01551f3b227d86cd9e
Ken Hare <kenhare@ultrapresence.com>
2019-08-15 01:41:32 -0600

    change language, clean up divs, add classes

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -40,82 +40,75 @@ class Inputs extends Component {
   render() {
     return (
       <form className="input-form">
-        <div style={{ flex: "0 1 22%", order: 2 }}>
-          <div className="field-label-container">
-            <label
-              className="field-label"
-              htmlFor="categoryId"
-              style={{
-                order: 1
-              }}
-            >
-              Item Category
-            </label>
-            <select
-              className="contents-category"
-              id="categoryId"
-              name="categoryId"
-              value={this.state.categoryId}
-              onChange={this.handleInputChange}
-            >
-              <option value="0">Select Category...</option>
-              <option value="1">Jewellery</option>
-              <option value="2">Collectibles</option>
-              <option value="3">Musical Instruments</option>
-              <option value="4">Sports Equipment</option>
-              <option value="5">Tools</option>
-            </select>
-          </div>
+        <div className="field-label-container field-name">
+          <label
+            className="field-label"
+            htmlFor="name"
+            style={{
+              order: 2
+            }}
+          >
+            Name
+          </label>
+          <input
+            className="contents-itemname"
+            id="name"
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
         </div>
-        <div style={{ flex: "0 1 31%", order: 2 }}>
-          <div className="field-label-container">
-            <label
-              className="field-label"
-              htmlFor="name"
-              style={{
-                order: 2
-              }}
-            >
-              Item Name
-            </label>
-            <input
-              className="contents-itemname"
-              id="name"
-              name="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleInputChange}
-            />
-          </div>
+        <div className="field-label-container field-category">
+          <label
+            className="field-label"
+            htmlFor="categoryId"
+            style={{
+              order: 1
+            }}
+          >
+            Category
+          </label>
+          <select
+            className="contents-category"
+            id="categoryId"
+            name="categoryId"
+            value={this.state.categoryId}
+            onChange={this.handleInputChange}
+          >
+            <option value="0">Select...</option>
+            <option value="1">Jewellery</option>
+            <option value="2">Collectibles</option>
+            <option value="3">Musical Instruments</option>
+            <option value="4">Sports Equipment</option>
+            <option value="5">Tools</option>
+          </select>
         </div>
-        <div style={{ flex: "0 1 20%", order: 3 }}>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <label
-              className="field-label"
-              htmlFor="amount"
-              style={{
-                order: 4
-              }}
-            >
-              Item Value (e.g. 3420)
-            </label>
 
-            <input
-              className="contents-amount"
-              name="amount"
-              id="amount"
-              type="number"
-              value={this.state.amount > 0 ? this.state.amount : ""}
-              onChange={this.handleInputChange}
-            />
-          </div>
+        <div className="field-label-container field-amount">
+          <label
+            className="field-label"
+            htmlFor="amount"
+            style={{
+              order: 4
+            }}
+          >
+            Amount{" "}
+            <span className="field-helper-text">(to nearest dollar)</span>
+          </label>
+
+          <input
+            className="contents-amount"
+            name="amount"
+            id="amount"
+            type="number"
+            value={this.state.amount > 0 ? this.state.amount : ""}
+            onChange={this.handleInputChange}
+          />
         </div>
-        <button
-          style={{ flex: "0 1 15%", order: 3 }}
-          className="contents-add"
-          onClick={this.handleSubmit}
-        >
-          Add Item{" "}
+
+        <button className="contents-add" onClick={this.handleSubmit}>
+          Add
         </button>
       </form>
     );
