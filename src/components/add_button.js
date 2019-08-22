7f0c0f3ee3d81c651827d074591b02bbcc5cc681
Ken Hare <kenhare@ultrapresence.com>
2019-08-22 16:29:50 -0600

    Changes to styles

--- src/components/add_button.js
+++ src/components/add_button.js
@@ -3,22 +3,19 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   @media (device-width: 411px) and (device-height: 731px) {
-    align-self: flex-end;
     font-size: 1rem;
     order: ${props => props.order};
-    padding: 5px;
-    margin-bottom: 10px;
-    color: white;
-    flex: 0 0 20%;
+    height: 25px;
+    margin: 5px 5px 10px;
+    width: 50px;
   }
   @media (min-width: 412px) {
-    ]align-self: center;
-    width: 10%;
     font-size: 1.3rem;
+    align-self: center;
+    width: 10%;
     padding: 5px;
     margin-top: 10px;
   }
-  border-width: 1px;
   background: #ffc240;
   font-weight: bold;
   color: white;
