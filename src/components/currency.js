ecce82d6f7f1e0bd610a76700ee2b423e28aa35e
Ken Hare <kenhare@ultrapresence.com>
2019-08-31 22:18:35 -0600

    add a  currencyString parameter, comments

--- src/components/currency.js
+++ src/components/currency.js
@@ -1,7 +1,15 @@
-export default function Currency({ value }) {
+/**
+ * Formats a number passed as {value:number}
+ * as a string and adds a currency symbol
+ * @export
+ * @param {value:Number, [currencySymbol:String | default "$"]} { value, currencySymbol = "$" }
+ * @returns String
+ */
+export default function Currency({ value, currencySymbol = "$" }) {
+  debugger;
   //Add thousand separators and currency symbol
   return (
-    "$" +
+    currencySymbol +
     value.toLocaleString(navigator.language, {
       minimumFractionDigits: 0,
       maximumFractionDigits: 0
