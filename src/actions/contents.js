0c611795a2a5e2213522dcc17ff984c38af591f5
Ken Hare <kenhare@ultrapresence.com>
2019-09-05 01:54:09 -0600

    Add REMOVE_ALL Action

--- src/actions/contents.js
+++ src/actions/contents.js
@@ -1,6 +1,6 @@
 export const ADD_ITEM = "ADD_ITEM";
 export const REMOVE_ITEM = "REMOVE_ITEM";
-
+export const REMOVE_ALL = "REMOVE_ALL";
 export function addItem(item) {
   return {
     type: ADD_ITEM,
@@ -11,5 +11,11 @@ export function removeItem(item) {
   return {
     type: REMOVE_ITEM,
     payload: item
+  };
+}
+export function removeAll(category) {
+  return {
+    type: REMOVE_ALL,
+    payload: category
   };
 }
